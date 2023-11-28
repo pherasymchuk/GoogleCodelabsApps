f5642a8ac9544e0a0249c3ca7c17074e6dae21e1
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2023-11-28 19:40:41 +0200

    Add Ukrainian translation

--- CupCake/app/src/main/java/com/example/cupcake/StartFragment.kt
+++ CupCake/app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -39,7 +39,7 @@ class StartFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
@@ -61,8 +61,7 @@ class StartFragment : Fragment() {
      */
     fun orderCupcake(quantity: Int) {
         sharedViewModel.setQuantity(quantity)
-        this.findNavController()
-            .navigate(StartFragmentDirections.actionStartFragmentToFlavorFragment2())
+        findNavController().navigate(StartFragmentDirections.actionStartFragmentToFlavorFragment2())
     }
 
     /**
