8e4741ad4c556ca58d95d5999e31b6cd2dced551
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2023-12-26 14:57:27 +0200

    Implement network service

--- Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -15,15 +15,37 @@
  */
 package com.example.amphibians.network
 
-// TODO: Create a property for the base URL provided in the codelab
-
-// TODO: Build the Moshi object with Kotlin adapter factory that Retrofit will be using to parse JSON
-
-// TODO: Build a Retrofit object with the Moshi converter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 interface AmphibianApiService {
-    // TODO: Declare a suspended function to get the list of amphibians
+    @GET("android-basics-kotlin-unit-4-pathway-2-project-api.json")
+    suspend fun getAmphibians(): List<Amphibian>
 }
 
-// TODO: Create an object that provides a lazy-initialized retrofit service
+interface Api {
+    val retrofitService: AmphibianApiService
 
+    object Amphibians : Api {
+        private val moshi: Moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        private val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+        override val retrofitService: AmphibianApiService by lazy {
+            retrofit.create(AmphibianApiService::class.java)
+        }
+    }
+
+    private companion object {
+        const val BASE_URL =
+            "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
+    }
+}
