525a67072cd53d3bc498174ea10fa56d44520c2c
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-01-03 16:44:35 +0200

    Add SettingsDataStore

--- Words/app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ Words/app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -1,3 +1,43 @@
 package com.example.wordsapp.data
 
-class SettingsDataStore
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class SettingsDataStore(context: Context) {
+    private val isLinearLayoutManager = booleanPreferencesKey("is_linear_layout_manager")
+    val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
+        .map { preferences ->
+            preferences[isLinearLayoutManager] ?: true
+        }
+
+    suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[this.isLinearLayoutManager] = isLinearLayoutManager
+        }
+    }
+
+    private companion object {
+        private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
+
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+            name = LAYOUT_PREFERENCES_NAME
+        )
+    }
+}
