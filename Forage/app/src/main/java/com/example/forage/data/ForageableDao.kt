c98f1ca269f058e36436bb1d609a4b3bb96fe56d
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-01-05 11:13:10 +0200

    Implement ForageableDao

--- Forage/app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ Forage/app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -16,21 +16,31 @@
 package com.example.forage.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.forage.model.Forageable
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access Object for database interaction.
  */
 @Dao
 interface ForageableDao {
+    @Query("SELECT * FROM forageable ORDER BY name ASC")
+    fun getAll(): Flow<List<Forageable>>
 
-    // TODO: implement a method to retrieve all Forageables from the database
+    @Query("SELECT * FROM forageable WHERE id = :id")
+    suspend fun getById(id: Int): Forageable
 
-    // TODO: implement a method to retrieve a Forageable from the database by id
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(forageable: Forageable)
 
-    // TODO: implement a method to insert a Forageable into the database
-    //  (use OnConflictStrategy.REPLACE)
+    @Update
+    suspend fun update(forageable: Forageable)
 
-    // TODO: implement a method to update a Forageable that is already in the database
-
-    // TODO: implement a method to delete a Forageable from the database.
+    @Delete
+    suspend fun delete(forageable: Forageable)
 }
