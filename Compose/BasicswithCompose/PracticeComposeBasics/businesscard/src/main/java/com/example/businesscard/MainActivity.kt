22af98b2cd1c5407ed39f62b74240672096957de
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-02-03 21:58:16 +0200

    Add Business Card app module

---
+++ Compose/BasicswithCompose/PracticeComposeBasics/businesscard/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.businesscard.ui.theme.BasicsWithComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsWithComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BasicsWithComposeTheme {
+        BusinessCard()
+    }
+}
