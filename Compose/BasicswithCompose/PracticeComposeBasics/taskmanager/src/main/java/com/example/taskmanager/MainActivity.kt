e7957c98f66280d949c4fdd77dcc81ec6ee652c6
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-02-03 18:13:12 +0200

    feat: finished Task manager app

--- Compose/BasicswithCompose/PracticeComposeBasics/taskmanager/src/main/java/com/example/taskmanager/MainActivity.kt
+++ Compose/BasicswithCompose/PracticeComposeBasics/taskmanager/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,23 +3,42 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.BasicsWithComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             BasicsWithComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Scaffold { innerPadding ->
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        TaskManagerScreen()
+                    }
                 }
             }
         }
@@ -27,17 +46,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskManagerScreen(modifier: Modifier = Modifier) {
+    val taskCompletedImage = painterResource(id = R.drawable.ic_task_completed)
+    Column(modifier = modifier.wrapContentSize()) {
+        Image(painter = taskCompletedImage, contentDescription = null)
+        Text(
+            text = "All task completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicsWithComposeTheme {
-        Greeting("Android")
+        TaskManagerScreen()
     }
 }
