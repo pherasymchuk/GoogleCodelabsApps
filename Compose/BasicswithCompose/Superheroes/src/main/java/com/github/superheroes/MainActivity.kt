ee671d44e8f167ef255ad6d690693593976e6477
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-02-16 11:33:27 +0200

    feat: implement model

--- Compose/BasicswithCompose/Superheroes/src/main/java/com/github/superheroes/MainActivity.kt
+++ Compose/BasicswithCompose/Superheroes/src/main/java/com/github/superheroes/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,30 +21,19 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    @Composable
-    fun SuperheroesApp() {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting(
-                name = "Android",
-                modifier = Modifier.padding(innerPadding)
-            )
-        }
-    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SuperheroesApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicsWithComposeTheme {
-        Greeting("Android")
+        SuperheroesApp()
     }
 }
