8338b5c2ba8becb9a0cafe6da219eb9a2f3ac4f4
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-04-16 10:57:11 +0300

    test: add kotlinx-coroutines-test dependency

--- Compose/BasicswithCompose/RaceTracker/build.gradle.kts
+++ Compose/BasicswithCompose/RaceTracker/build.gradle.kts
@@ -47,7 +47,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -63,4 +62,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
