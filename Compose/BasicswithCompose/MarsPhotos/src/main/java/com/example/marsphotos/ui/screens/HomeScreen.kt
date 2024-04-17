0e04cc38170d0b8f3c8ec6d8dc44313af70a4360
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-04-17 17:14:27 +0300

    feat: add Exception handling

--- Compose/BasicswithCompose/MarsPhotos/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ Compose/BasicswithCompose/MarsPhotos/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -15,13 +15,27 @@
  */
 package com.example.marsphotos.ui.screens
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,11 +44,59 @@ import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
-    marsUiState: String,
+    marsUiState: MarsUiState,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
-    ResultScreen(marsUiState, modifier.padding(top = contentPadding.calculateTopPadding()))
+    when (marsUiState) {
+        is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is MarsUiState.Success -> ResultScreen(
+            marsUiState.photos,
+            modifier
+                .padding(top = contentPadding.calculateTopPadding())
+                .fillMaxWidth()
+        )
+
+        is MarsUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val angle = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = "loadingData"
+    )
+    Image(
+        modifier = Modifier
+            .size(200.dp)
+            .graphicsLayer(rotationZ = angle.value),
+        painter = painterResource(id = R.drawable.loading_img),
+        contentDescription = stringResource(id = R.string.loading)
+    )
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(id = R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 /**
@@ -50,10 +112,26 @@ fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
+@Composable
+private fun LoadingScreenPreview() {
+    MarsPhotosTheme {
+        LoadingScreen()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ResultScreenPreview() {
     MarsPhotosTheme {
         ResultScreen(stringResource(R.string.placeholder_result))
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ErrorScreenPreview() {
+    MarsPhotosTheme {
+        ErrorScreen(modifier = Modifier.fillMaxSize())
     }
 }
