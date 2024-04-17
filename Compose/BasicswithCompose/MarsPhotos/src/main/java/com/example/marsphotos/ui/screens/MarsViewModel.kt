d41337d45973726c9104ac0c9f848d455b3d8a56
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-04-17 13:07:10 +0300

    feat: add Retrofit web service

--- Compose/BasicswithCompose/MarsPhotos/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ Compose/BasicswithCompose/MarsPhotos/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -19,6 +19,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
 
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
@@ -37,6 +40,9 @@ class MarsViewModel : ViewModel() {
      * [MarsPhoto] [List] [MutableList].
      */
     fun getMarsPhotos() {
-        marsUiState = "Set the Mars API status response here!"
+        viewModelScope.launch {
+            val listResult = MarsApi.retrofitService.getPhotos()
+            marsUiState = listResult
+        }
     }
 }
