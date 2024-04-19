414fc3e484c82a0f5e76654b9aee543262c27271
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-04-19 11:28:37 +0300

    build: add kotlinx-coroutines-test dependency

--- Compose/BasicswithCompose/MarsPhotos/build.gradle.kts
+++ Compose/BasicswithCompose/MarsPhotos/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
