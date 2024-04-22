6e3ad0ffe0b238051da6f201018f80e53e67191a
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-04-22 12:32:14 +0300

    build: add Coil

--- Compose/BasicswithCompose/MarsPhotos/build.gradle.kts
+++ Compose/BasicswithCompose/MarsPhotos/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit.jupiter.api)
+    // https://mvnrepository.com/artifact/io.coil-kt/coil-compose
+    runtimeOnly(libs.coil.compose)
     testRuntimeOnly(libs.junit.jupiter.engine)
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
