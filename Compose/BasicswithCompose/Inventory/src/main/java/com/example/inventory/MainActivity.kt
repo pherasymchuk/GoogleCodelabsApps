44a79fdac76340aedc6b7362f9334ea8c1e344cb
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-05-01 21:07:48 +0300

    feat: update deps, add Inventory app (compose)

---
+++ Compose/BasicswithCompose/Inventory/src/main/java/com/example/inventory/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.inventory
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.inventory.ui.theme.InventoryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InventoryTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    InventoryApp()
+                }
+            }
+        }
+    }
+}
