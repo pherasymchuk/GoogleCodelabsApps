e94341ca59b9e5e4de3bf2c377f11a0c04aaec88
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-05-20 12:48:03 +0300

    feat: add Flight Search app module

---
+++ Compose/BasicswithCompose/PracticeComposeBasics/FlightSearch/src/main/java/com/example/flightsearch/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.flightsearch
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.flightsearch.ui.theme.FlightSearchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FlightSearchTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun FlightSearchApp(modifier: Modifier = Modifier) {
+
+}
