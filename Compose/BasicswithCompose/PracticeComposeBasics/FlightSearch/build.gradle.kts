af24938f0916f787745d8bf105dd64c211207f52
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-05-23 17:23:50 +0300

    build: update kotlinx.serialization version to 1.7.0-RC to work with Kotlin 2.0

--- Compose/BasicswithCompose/PracticeComposeBasics/FlightSearch/build.gradle.kts
+++ Compose/BasicswithCompose/PracticeComposeBasics/FlightSearch/build.gradle.kts
@@ -51,11 +51,14 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.room.runtime)
     implementation(project(":DevUtils"))
+
+    implementation(libs.androidx.room.runtime)
     annotationProcessor(libs.androidx.room.compiler)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.core.ktx)
@@ -68,11 +71,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
