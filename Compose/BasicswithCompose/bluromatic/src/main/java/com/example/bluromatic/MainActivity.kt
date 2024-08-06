bba22cae9e5cf4d90f2a59121f4974589b99b3b0
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-08-06 20:42:57 +0300

    feat: add initial files for Bluromatic app
    This commit adds the initial files for the Bluromatic app, including:
    
    - AppContainer, Repository, ViewModel
    , and Application classes
    - BlurAmount data class and BlurAmountData object
    - WorkManagerBluromaticRepository class
    - WorkerUtils with functions for making status notifications, blurring bitmaps, and writing bitmaps to files
    - Constants for notification channels, image manipulation work, and other keys
    - BluromaticScreen compos
    able function for the app's UI
    - Updated themes and colors
    - Updated dependencies in `libs.versions.toml` and `build.gradle.kts`
    
    This lays the foundation for the Bluromatic app, which will allow users to select an image and apply a blur effect to it.

---
+++ Compose/BasicswithCompose/bluromatic/src/main/java/com/example/bluromatic/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.bluromatic
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.bluromagic.R
+import com.example.bluromatic.ui.BluromaticScreen
+import com.example.bluromatic.ui.theme.BluromaticTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BluromaticTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BluromaticScreen()
+                }
+            }
+        }
+    }
+}
+
+fun Context.getImageUri(): Uri {
+    val resources = this.resources
+
+    return Uri.Builder()
+        .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+        .authority(resources.getResourcePackageName(R.drawable.android_cupcake))
+        .appendPath(resources.getResourceTypeName(R.drawable.android_cupcake))
+        .appendPath(resources.getResourceEntryName(R.drawable.android_cupcake))
+        .build()
+}
