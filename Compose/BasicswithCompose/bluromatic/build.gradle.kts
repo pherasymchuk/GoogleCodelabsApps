f2f27b1471a63a2336b2dd8c1afb2ac13873917a
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-08-07 13:00:18 +0300

    chore: update compile and target SDK versions
    This commit updates the compile and target SDK versions for the Bluromatic app to 35.

--- Compose/BasicswithCompose/bluromatic/build.gradle.kts
+++ Compose/BasicswithCompose/bluromatic/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.bluromatic"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.bluromatic"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
