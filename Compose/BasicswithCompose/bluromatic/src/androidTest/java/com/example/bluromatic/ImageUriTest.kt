de6b1354d22f3258ee1755d18da28b9f6dd06744
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-08-08 18:12:43 +0300

    test: remove redundant test
    Removed a redundant test that checked the default drawable URI, as it was already covered by other tests.

--- Compose/BasicswithCompose/bluromatic/src/androidTest/java/com/example/bluromatic/ImageUriTest.kt
+++ Compose/BasicswithCompose/bluromatic/src/androidTest/java/com/example/bluromatic/ImageUriTest.kt
@@ -11,19 +11,6 @@ class ImageUriTest {
     private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 
     @Test
-    fun uri_isDefaultDrawable() {
-        val expectedUri = Uri.Builder()
-            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-            .authority(appContext.resources.getResourcePackageName(R.drawable.android_cupcake))
-            .appendPath(appContext.resources.getResourceTypeName(R.drawable.android_cupcake))
-            .appendPath(appContext.resources.getResourceEntryName(R.drawable.android_cupcake))
-            .build()
-
-        val imageUri = ImageUri.Drawable(appContext)
-        Assert.assertEquals(expectedUri, imageUri.uri())
-    }
-
-    @Test
     fun uri_isCustomDrawable() {
         val customDrawableId: Int = R.drawable.ic_launcher_background
         val expectedUri = Uri.Builder()
