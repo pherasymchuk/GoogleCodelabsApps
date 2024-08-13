97b0141d9115e6afc3a560b9e40d7c0975082454
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-08-13 14:03:50 +0300

    feat: enable dynamic color
    This commit enables dynamic color support in the Bluromatic theme by setting the `dynamicColor` parameter to `true`. This allows the
     app to adapt its colors to the user's system theme on Android 12 and above.

--- Compose/BasicswithCompose/bluromatic/src/main/java/com/example/bluromatic/ui/theme/Theme.kt
+++ Compose/BasicswithCompose/bluromatic/src/main/java/com/example/bluromatic/ui/theme/Theme.kt
@@ -32,7 +32,7 @@ private val LightColorScheme = lightColorScheme(
 fun BluromaticTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
