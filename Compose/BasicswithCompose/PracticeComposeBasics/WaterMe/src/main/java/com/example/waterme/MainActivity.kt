bd9392f03f83f444b3072ce21c7718bb87d1b0e4
Pavlo Herasymchuk <herasymchuk.pavlo@gmail.com>
2024-08-22 12:36:48 +0300

    feat: add initial files for WaterMe app
    This commit adds the initial files for the WaterMe app, including:
    
    - AppContainer, Repository, ViewModel
    , and Application classes
    - Plant data class
    - WorkManagerWaterRepository class
    - WorkerUtils with functions for making status notifications
    - Constants for notification channels, image manipulation work, and other keys
    - WaterMeApp composable function for the app's UI
    - Updated themes and colors
    - Updated dependencies
     in `libs.versions.toml` and `build.gradle.kts`
    
    This lays the foundation for the WaterMe app, which will allow users to schedule reminders to water their plants.

---
+++ Compose/BasicswithCompose/PracticeComposeBasics/WaterMe/src/main/java/com/example/waterme/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.waterme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.waterme.ui.WaterMeApp
+import com.example.waterme.ui.theme.BasicsWithComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BasicsWithComposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    WaterMeApp()
+                }
+            }
+        }
+    }
+}
+
